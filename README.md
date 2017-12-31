33c75dd87a65378448f87e409982810f8fa5601d
Dennis Brinkman <dennisbrinkman93@gmail.com>
2017-12-31 22:20:09 +0100

    Progress

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-#Spaceflight Simulator Configuration File Parser (WIP)
+##Spaceflight Simulator Configuration File Interpreter (WIP)
 This is my attempt at making an interpreter for the configuration file of the
-flight simulator project that I'm working on. It is also my attempt at learnign
+flight simulator project that I'm working on. It is also my attempt at learning
 the principles behind an interpreter, so please forgive me if it looks bad.
 
-### Nowhere near finished!!
+### Nowhere near finished!!!
--- src/Tokenizer.jl
+++ src/Tokenizer.jl
@@ -1,10 +1,11 @@
-path = string(pwd(), "\\src\\FirstTest.txt")
+#This file contains the Tokenizer of the Configuration file interpreter
 
 const TokenTypeDict = Dict{String, Int64}("LEFT_PAREN" => 1, "RIGHT_PAREN" => 2, "LEFT_BRACE" => 3,
             "RIGHT_BRACE" => 4, "COMMA" => 5, "DOT" => 6, "MINUS" => 7, "PLUS" => 8,
             "SEMICOLON" => 9, "COLON" => 10, "SLASH" => 11, "STAR" => 12, "BANG" => 13,
             "BANG_EQUAL" => 14, "EQUAL" => 15, "EQUAL_EQUAL" => 16, "GREATER" => 17,
-            "GREATER_EQUAL" => 18, "LESS" => 19, "LESS_EQUAL" => 20,
+            "GREATER_EQUAL" => 18, "LESS" => 19, "LESS_EQUAL" => 20, "LEFT_BRACKET" => 51,
+            "RIGHT_BRACKET" => 52,
 
             #Literals
             "IDENTIFIER" => 21, "STRING" => 22, "NUMBER" => 23, "UNIT" => 24,
@@ -17,13 +18,28 @@ const TokenTypeDict = Dict{String, Int64}("LEFT_PAREN" => 1, "RIGHT_PAREN" => 2,
             #INITIALSTATE,
 
             #KeyWords of SolarSystem
-            "SOLARSYTEM" => 35, "BARYCENTER" => 36, "STAR" => 37, "PARENT" => 38,
+            "SOLARSYSTEM" => 35, "BARYCENTER" => 36, "STAR" => 37, "PARENT" => 38,
             "MASS" => 39, "RADIUS" => 40, "LUMINOUSITY" => 41, "STATE" => 42,
             "PLANETMOONBARYCENTER" => 43, "PLANET" => 44, "ALBEDO" => 45, "ATMOSPHERE" => 46,
-            "GRAVITY" => 47, "SHAPE" => 48, "MOON" => 49,
+            "GRAVITY" => 47, "SHAPE" => 48, "MOON" => 49, "SPKKERNEL" => 50,
 
             #End of File
-            "EOF" => 50)
+            "EOF" => 53)
+
+Keywords = Dict{String,String}("vehicle" => "VEHICLE", "drymass" => "DRYMASS",
+            "fuelmass" => "FUELMASS", "referencearea" => "REFERENCEAREA",
+            "referencelengthlon" => "REFERENCELENGTHLON", "referencelengthlat" =>
+            "REFERENCELENGTHLAT", "reflectioncoeff" => "REFLECTIONCOEFF",
+            "srparea" => "SRPAREA", "inertiatensor" => "INERTIATENSOR",
+            "invinertiatensor" => "INVINERTIATENSOR",
+
+            #SolarSystem Keywords
+            "solarsystem" => "SOLARSYSTEM", "barycenter" => "BARYCENTER", "star" => "STAR",
+            "parent" => "PARENT", "mass" => "MASS", "radius" => "RADIUS",
+            "luminousity" => "LUMINOUSITY", "state" => "STATE",
+            "planetmoonbarycenter" => "PLANETMOONBARYCENTER", "planet" => "PLANET",
+            "albedo" => "ALBEDO", "atmosphere" => "ATMOSPHERE", "gravity" => "GRAVITY",
+            "shape" => "SHAPE", "moon" => "MOON", "spkkernel" => "SPKKERNEL")
 
 #Create TokenType structure, which also checks if the potential Token actually exists
 struct TokenType
@@ -43,11 +59,11 @@ end #TokenType
 struct Token
     token::TokenType
     lexeme::String
-    literal::String
+    literal::Any
     line::Int64
 
     #Make constructor
-    function Token(token::String , lexeme::String, literal::String, line::Int64)
+    function Token(token::String , lexeme::String, literal::Any, line::Int64)
         newtoken = TokenType(token)
         return new(newtoken, lexeme, literal, line)
     end  #Constructor
@@ -59,11 +75,14 @@ mutable struct Tokenizer
     start::Int64
     current::Int64
     line::Int64
+    isalphanumeric::Function
     isAtEnd::Function
     peek::Function
     peeknext::Function
     advance::Function
     match::Function
+    number::Function
+    identifier::Function
     addToken1::Function
     addToken2::Function
     scanToken::Function
@@ -82,6 +101,11 @@ mutable struct Tokenizer
         this.line = 1
         this.tokens = Vector{Token}()
 
+        #make isalphanumeric function
+        this.isalphanumeric = function (c::Char)
+            return isalpha(c) || isdigit(c)
+        end #isalphanumeric
+
         #make isAtEnd function
         this.isAtEnd = function ()
             return this.current >= length(this.source)
@@ -98,7 +122,11 @@ mutable struct Tokenizer
 
         #make peeknext function
         this.peeknext = function ()
-
+            if (this.current + 1) >= length(this.source)
+                return '\0'
+            else
+                return this.source[this.current + 1]
+            end
         end #peeknext
 
         #make advance function
@@ -117,14 +145,49 @@ mutable struct Tokenizer
             end
         end #match
 
+        #make number function
+        this.number = function ()
+            while isdigit(this.peek())
+                this.advance()
+            end
+
+            #look for fractional part
+            if this.peek() == '.' && isdigit(this.peeknext())
+                this.advance()
+                while isdigit(this.peek())
+                    this.advance()
+                end
+            end
+
+            this.addToken2("NUMBER", parse(this.source[this.start:this.current-1]))
+            return nothing
+        end #number
+
+        #make identifier function
+        this.identifier = function ()
+            while this.isalphanumeric(this.peek())
+                this.advance()
+            end
+            #See if identifier is keywords
+            text = lowercase(this.source[this.start:this.current-1])
+            if haskey(Keywords, text)
+                this.addToken1(Keywords[text])
+            else
+                this.addToken1("IDENTIFIER")
+            end
+            return nothing
+        end #identifier
+
         #make addToken functions
         this.addToken1 = function (token::String)
             this.addToken2(token, "null")
+            return nothing
         end #addToken
 
-        this.addToken2 = function (token::String, literal::String)
+        this.addToken2 = function (token::String, literal::Any)
             text = this.source[this.start:this.current-1]
             push!(this.tokens, Token(token, text, literal, this.line))
+            return nothing
         end
 
         #make ScanToken function
@@ -139,6 +202,10 @@ mutable struct Tokenizer
                 this.addToken1("LEFT_BRACE")
             elseif c == '}'
                 this.addToken1("RIGHT_BRACE")
+            elseif c == '['
+                this.addToken1("LEFT_BRACKET")
+            elseif c == ']'
+                this.addToken1("RIGHT_BRACKET")
             elseif c == ','
                 this.addToken1("COMMA")
             elseif c == '.'
@@ -178,9 +245,15 @@ mutable struct Tokenizer
             elseif c == '\n'
                 this.line += 1
             else
-                error("Unidentified Token Found in line $(this.line), column $(this.current)")
+                if isdigit(c)
+                    this.number()
+                elseif isalpha(c)
+                    this.identifier()
+                else
+                    error("Unidentified Token Found in line $(this.line)")
+                end
             end
-
+            return nothing
         end #scanToken
 
         this.tokenize = function()
@@ -194,7 +267,20 @@ mutable struct Tokenizer
             push!(this.tokens,Token("EOF", "", "null", this.line))
             return nothing
         end #tokenize
-
+        #Must return the created instance
         return this
     end #Constructor
 end# Tokenizer
+
+#For Testing Purposes Only
+path = string(pwd(), "\\src\\FullTest.txt")
+TimeVector = Vector{Float64}()
+tt = Tokenizer(path)
+tt.tokenize()
+for i=1:1000
+    tic()
+    tt = Tokenizer(path)
+    tt.tokenize()
+    push!(TimeVector,toq())
+end
+println(mean(TimeVector))
